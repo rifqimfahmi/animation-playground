0ba143405b38d3835f10c1ba71fa6aa0e205a17c
Rifqi M Fahmi <rifqi416@gmail.com>
2019-08-10 09:04:43 +0700

    create item value animator

--- app/src/main/java/com/rifqimfahmi/animationplayground/animations/a01/viewholder/ItemValueAnimator.kt
+++ app/src/main/java/com/rifqimfahmi/animationplayground/animations/a01/viewholder/ItemValueAnimator.kt
@@ -1,6 +1,8 @@
 package com.rifqimfahmi.animationplayground.animations.a01.viewholder
 
+import android.animation.ValueAnimator
 import android.view.View
+import android.widget.TextView
 import com.rifqimfahmi.animationplayground.R
 import com.rifqimfahmi.animationplayground.animations.a00_base.adapter.BaseViewHolder
 import com.rifqimfahmi.animationplayground.animations.a01.model.ModelValueAnimator
@@ -10,8 +12,20 @@ import com.rifqimfahmi.animationplayground.animations.a01.model.ModelValueAnimat
  */
 
 class ItemValueAnimator(itemView: View) : BaseViewHolder<ModelValueAnimator>(itemView) {
-    override fun bind(model: ModelValueAnimator) {
 
+    private val placeHolder = itemView.findViewById<TextView>(R.id.tv_placeholder)
+
+    override fun bind(model: ModelValueAnimator) {
+        val animator = ValueAnimator.ofInt(0, 50000).apply {
+            duration = 1000
+            addUpdateListener { valueAnimator ->
+                placeHolder.text = valueAnimator.animatedValue.toString()
+            }
+        }
+
+        itemView.setOnClickListener {
+            animator.start()
+        }
     }
 
     companion object {
